288d1f0f31c646b8cf2fadf6059a55dd19316d31
Paweł Chudecki <pawelc29@interia.pl>
2026-02-05 14:41:48 +0100

    user picture

--- models/userModel.js
+++ models/userModel.js
@@ -6,8 +6,9 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true },
     password: { type: String, required: true },
     admin: { type: Boolean },
+    picture: { type: String },
   },
-  { timestamps: true }
+  { timestamps: true },
 );
 
 module.exports = mongoose.model("User", userSchema);
